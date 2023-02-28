8f7e7a337f8cd75199a1a5568b187677c1e3acfe
Mrk0p <sachinwele23@gmail.com>
2023-02-28 13:10:00 +0530

    Added:indian state census analyser

---
+++ src/test/java/com/bridgelabz/census_analyser/CensusAnalyserTest.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.census_analyser;
+
+import com.bridgelabaz.census_analyser.CensusAnalyser;
+import com.bridgelabaz.census_analyser.CensusAnalyserException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class CensusAnalyserTest {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\sachi\\IdeaProjects\\Day29-indian_state\\src\\main\\java\\com\\bridgelabaz\\census_analyser\\IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+}
